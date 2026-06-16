3b2dbcf7fb8fb2f077db658f574f0dee8975f412
Jamie <j.tran1@students.htl-leonding.ac.at>
2026-06-16 13:59:35 +0200

    Change

--- src/Game.java
+++ src/Game.java
@@ -2,7 +2,7 @@ public class Game {
     public static void main() {
         System.out.println("Hello, Benjamin und Gerald!");
         System.out.println("Jamie is ein Picture!!!");
-        System.out.println("Gerald mag Denia!!!");
+        System.out.println("Gerald mag Denia(DIHHHHH)!!!");
         System.out.println("HELLLLLLLOOOO");
         System.out.println("Jamie hat einen Kleinen ;)");
     }
