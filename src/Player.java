5dcb7143925097cad4fd23752f87b5552d5c0e93
Benjamin Hartmann <benjamin.p.hartmann@gmail.com>
2026-06-16 14:18:06 +0200

    progress

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
+    private int balance;
+    private final List<InventoryItem> inventory;
+
+    public Player() {
+        this.balance = 0;
+        this.inventory = new ArrayList<>();
+
+        for (ProductType currentType : ProductType.values()) {
+            inventory.add(new InventoryItem(currentType));
+        }
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void addMoney(int amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Amount may not be negative");
+        }
+
+        balance += amount;
+    }
+
+    public boolean removeMoney(int amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            return true;
+        }
+
+        return false;
+    }
+
+    public void addItem(ProductType type, int amount) {
+        for (InventoryItem item : inventory) {
+            if (item.getProduct() == type) {
+                item.add(amount);
+            }
+        }
+    }
 }
