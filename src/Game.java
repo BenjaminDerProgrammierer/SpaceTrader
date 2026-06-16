ee4de67724720ab9751b0060c080504b82c91458
Gerald <g.redl@students.htl-leonding.ac.at>
2026-06-16 13:51:19 +0200

    Change

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,7 @@
 public class Game {
     public static void main() {
         System.out.println("Hello, Benjamin und Gerald!");
+        System.out.println("Jamie is ein Pic!!!");
         System.out.println("HELLLLLLLOOOO");
     }
 }
