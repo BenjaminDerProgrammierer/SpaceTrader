64816e9b9635e442686b53e03f4ed66ecb17408b
Benjamin Hartmann <benjamin.p.hartmann@gmail.com>
2026-06-22 10:26:46 +0200

    Add rollPrice

--- src/MarketItem.java
+++ src/MarketItem.java
@@ -1,7 +1,18 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class MarketItem extends InventoryItem {
     private int price;
 
     MarketItem(ProductType product) {
         super(product);
+        rollPrice();
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void rollPrice() {
+        this.price = ThreadLocalRandom.current().nextInt(getProduct().getMinPrice(), getProduct().getMaxPrice() + 1);;
     }
 }
