6e36e60e63dd456d62a9c2fc5966832ef978e662
Benjamin Hartmann <benjamin.p.hartmann@gmail.com>
2026-06-22 10:07:32 +0200

    Bugfixes

--- src/Game.java
+++ src/Game.java
@@ -3,11 +3,11 @@ private final List<Market> markets = new LinkedList<>();
 
 void main() {
     // https://patorjk.com/software/taag/#p=display&f=Rectangles&t=SpaceTrader&x=none&v=4&h=4&w=80&we=false
-    IO.println(" _____                 _____           _         ");
-    IO.println("|   __|___ ___ ___ ___|_   _|___ ___ _| |___ ___ ");
-    IO.println("|__   | . | .'|  _| -_| | | |  _| .'| . | -_|  _|");
-    IO.println("|_____|  _|__,|___|___| |_| |_| |__,|___|___|_|  ");
-    IO.println("      |_|                                        ");
+    System.out.println(" _____                 _____           _         ");
+    System.out.println("|   __|___ ___ ___ ___|_   _|___ ___ _| |___ ___ ");
+    System.out.println("|__   | . | .'|  _| -_| | | |  _| .'| . | -_|  _|");
+    System.out.println("|_____|  _|__,|___|___| |_| |_| |__,|___|___|_|  ");
+    System.out.println("      |_|                                        ");
 
     // TODO: Markets
     // TODO: Game loop
