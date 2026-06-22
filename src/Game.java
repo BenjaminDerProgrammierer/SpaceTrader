789520efe4246b73c2352dcf4176f3604cf1bf82
Benjamin Hartmann <benjamin.p.hartmann@gmail.com>
2026-06-22 10:50:26 +0200

    Add game loop and todos

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,8 @@
+private static final int GAME_ROUNDS = 30;
+
 private final Player player = new Player();
 private final List<Market> markets = new LinkedList<>();
+private Market currentMarket;
 
 void main() {
     // https://patorjk.com/software/taag/#p=display&f=Rectangles&t=SpaceTrader&x=none&v=4&h=4&w=80&we=false
@@ -9,9 +12,17 @@ void main() {
     System.out.println("|_____|  _|__,|___|___| |_| |_| |__,|___|___|_|  ");
     System.out.println("      |_|                                        ");
 
-    for (Market market : markets) {
-        market.rollRound();
+    // TODO: Populate markets
+
+    for (int i = 0; i < GAME_ROUNDS; i++) {
+        System.out.printf("Day: %d/%d - Balance: %d$ - Debt: %d$ - Inventory Space: %d/%d - City: %s%n", i + 1, GAME_ROUNDS, player.getBalance(), player.getDebt(), player.getItemCount(), Player.ITEM_CAPACITY, currentMarket);
+
+        // TODO: Menu
+        // Loan Shark: Pay off Debt (+10%/DAY)
+        // Bank: Store Money with interest (+2%/DAY)
+        // Private Jet: Change City
+        // Market: Buy/Sell items
+        // Roll Market prices and events
+        currentMarket.rollRound();
     }
-    // TODO: Markets
-    // TODO: Game loop
 }
--- src/Player.java
+++ src/Player.java
@@ -6,12 +6,29 @@ import java.util.List;
 
 public class Player {
     private static final int STARTING_MONEY = 200;
+    private static final int STARTING_DEBT = 5500;
+    public static final int ITEM_CAPACITY = 5500;
 
     private int balance = STARTING_MONEY;
+    private int debt = STARTING_DEBT;
     private final Inventory<InventoryItem> inventory = new Inventory<>(InventoryItem.class);
 
     public int getBalance() {
         return balance;
+    }
+
+    public int getDebt() {
+        return debt;
+    }
+
+    public int getItemCount() {
+        int count = 0;
+
+        for (InventoryItem item : inventory) {
+            count += item.getCount();
+        }
+
+        return count;
     }
 
     public void addMoney(int amount) {
