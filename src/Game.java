997a9d3d166c5c19c29df20ebf0d0627f09cc5a2
Benjamin Hartmann <benjamin.p.hartmann@gmail.com>
2026-06-22 09:41:34 +0200

    Make Game a CSF

--- src/Game.java
+++ src/Game.java
@@ -1,19 +1,14 @@
-import java.util.LinkedList;
-import java.util.List;
+private final Player player = new Player();
+private final List<Market> markets = new LinkedList<>();
 
-public class Game {
-    private final Player player = new Player();
-    private final List<Market> markets = new LinkedList<>();
+void main() {
+    // https://patorjk.com/software/taag/#p=display&f=Rectangles&t=SpaceTrader&x=none&v=4&h=4&w=80&we=false
+    IO.println(" _____                 _____           _         ");
+    IO.println("|   __|___ ___ ___ ___|_   _|___ ___ _| |___ ___ ");
+    IO.println("|__   | . | .'|  _| -_| | | |  _| .'| . | -_|  _|");
+    IO.println("|_____|  _|__,|___|___| |_| |_| |__,|___|___|_|  ");
+    IO.println("      |_|                                        ");
 
-    static void main(String[] args) {
-        // https://patorjk.com/software/taag/#p=display&f=Rectangles&t=SpaceTrader&x=none&v=4&h=4&w=80&we=false
-        System.out.println(" _____                 _____           _         ");
-        System.out.println("|   __|___ ___ ___ ___|_   _|___ ___ _| |___ ___ ");
-        System.out.println("|__   | . | .'|  _| -_| | | |  _| .'| . | -_|  _|");
-        System.out.println("|_____|  _|__,|___|___| |_| |_| |__,|___|___|_|  ");
-        System.out.println("      |_|                                        ");
-
-        // TODO: Markets
-        // TODO: Game loop
-    }
+    // TODO: Markets
+    // TODO: Game loop
 }
